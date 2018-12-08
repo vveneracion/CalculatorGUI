d5ea81bdaf47230e887a8751421f2879ff5ef121
Veronika-Lisse Veneracion <veronikaveneracion@gmail.com>
2018-12-07 22:29:32 -0500

    adding operators

--- CalculatorFrame.java
+++ CalculatorFrame.java
@@ -20,6 +20,9 @@ public class CalculatorFrame extends JFrame{ //implements ActionListener{
     private static final int CALCULATE_ROWS = 4;
     private static final int CALCULATE_COLUMNS = 20;
 
+    private double num1, num2; 
+    private String operation, temp;
+
     private JTextArea calculation;
 
     public CalculatorFrame(){
@@ -87,12 +90,10 @@ public class CalculatorFrame extends JFrame{ //implements ActionListener{
     class addNumButtonListener implements ActionListener{
 
         private JTextArea screen;
-        private String temp;
 
         public addNumButtonListener(JTextArea calculation){
 
             this.screen = calculation;
-            this.temp = "";
 
         }
 
@@ -145,6 +146,69 @@ public class CalculatorFrame extends JFrame{ //implements ActionListener{
 
     }
 
+    class addOperatorListener implements ActionListener{
+
+        private JTextArea screen;
+        
+        public addOperatorListener(JTextArea calculation){
+            this.screen = calculation;
+        }
+        
+        @Override
+        public void actionPerformed(ActionEvent event){
+
+            if(event.getActionCommand().equals("+")){
+                num1 = Double.parseDouble(screen.getText());
+                operation = "+";
+                screen.setText("");
+            }
+            else if(event.getActionCommand().equals("-")){
+                num1 = Double.parseDouble(screen.getText());
+                operation = "-";
+                screen.setText("");
+            }
+            else if(event.getActionCommand().equals("*")){
+                num1 = Double.parseDouble(screen.getText());
+                operation = "*";
+                screen.setText("");
+            }
+            else if(event.getActionCommand().equals("/")){
+                num1 = Double.parseDouble(screen.getText());
+                operation = "/";
+                screen.setText("");
+            }
+            else if(event.getActionCommand().equals("+/-")){
+                double curr = Double.parseDouble(screen.getText());
+                curr = curr*(-1);
+                screen.setText(String.valueOf(curr));
+            }
+            else if(event.getActionCommand().equals(".")){
+                temp = screen.getText() + ".";
+                screen.setText(temp);
+            }
+            else if(event.getActionCommand().equals("=")){
+                num2 = Double.parseDouble(screen.getText());
+                double answer = 0;
+                if(operation.equals("+")){
+                    answer = num1 + num2;
+                }
+                else if(operation.equals("-")){
+                    answer = num1 - num2;
+                }
+                else if(operation.equals("*")){
+                    answer = num1 * num2;
+                }
+                else if(operation.equals("/")){
+                    answer = num1 / num2;
+                }
+
+                screen.setText(String.valueOf(answer));
+            }
+
+        }
+        
+    }
+
     public JPanel createSecondRow(){
 
         JPanel secondRow = new JPanel(new GridLayout(1, 4, 10, 0));
@@ -164,6 +228,7 @@ public class CalculatorFrame extends JFrame{ //implements ActionListener{
     public JPanel createThirdRow(){
 
         JPanel thirdRow = new JPanel(new GridLayout(1, 4, 10, 0));
+        ActionListener opListener = new addOperatorListener(calculation);
 
         JButton nine = new JButton("9");
         ActionListener numListener = new addNumButtonListener(calculation);
@@ -171,7 +236,9 @@ public class CalculatorFrame extends JFrame{ //implements ActionListener{
         JButton zero = new JButton("0");
         zero.addActionListener(numListener);
         JButton plus = new JButton("+");
+        plus.addActionListener(opListener);
         JButton minus = new JButton("-");
+        minus.addActionListener(opListener);
 
         thirdRow.add(nine);
         thirdRow.add(zero);
@@ -185,11 +252,16 @@ public class CalculatorFrame extends JFrame{ //implements ActionListener{
     public JPanel createFourthRow(){
 
         JPanel fourthRow = new JPanel(new GridLayout(1, 4, 10, 0));
+        ActionListener opListener = new addOperatorListener(calculation);
 
         JButton multiply = new JButton("*");
+        multiply.addActionListener(opListener);
         JButton divide = new JButton("/");
+        divide.addActionListener(opListener);
         JButton posneg = new JButton("+/-");
+        posneg.addActionListener(opListener);
         JButton dot = new JButton(".");
+        dot.addActionListener(opListener);
 
         fourthRow.add(multiply);
         fourthRow.add(divide);
@@ -208,6 +280,8 @@ public class CalculatorFrame extends JFrame{ //implements ActionListener{
         ActionListener clearListener = new addClearButtonListener(calculation);
         clear.addActionListener(clearListener);
         JButton equals = new JButton("=");
+        ActionListener opListener = new addOperatorListener(calculation);
+        equals.addActionListener(opListener);
 
         fifthRow.add(clear);
         fifthRow.add(equals);
@@ -229,6 +303,10 @@ public class CalculatorFrame extends JFrame{ //implements ActionListener{
         public void actionPerformed(ActionEvent event){
 
             screen.setText("");
+            operation = "";
+            num1 = 0;
+            num2 = 0;
+            temp = "";
 
         }
     }
